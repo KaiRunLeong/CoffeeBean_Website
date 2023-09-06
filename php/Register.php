66eb0215f0fc3ab9dbcf69fdda1e0cd1f8b6143e
Kai Run Leong <S3862092@student.rmit.edu.au>
2023-09-06 15:00:28 +0800

    initial commit

---
+++ php/Register.php
@@ -0,0 +1,24 @@
+<?php
+include('Data_Access.php');
+?>
+
+<html>
+    <body>
+        <?php
+        $user_name = $_POST['username'];
+        $user_password = $_POST['userPassword'];
+        $user_email = $_POST["useremail"];
+        $user_first = $_POST["userFirstName"];
+        $user_last = $_POST["userLastName"];
+        $user_phone = $_POST["userPhoneNumber"];
+        $user_role = "user";
+
+        $db = openDB();
+        
+        createNewUser($db, $user_name, $user_password, $user_email, 
+            $user_first, $user_last, $user_phone, $user_role);
+
+        closeDB($db);
+        ?>
+    </body>
+</html>
---
+++ php/getData.php
@@ -0,0 +1,32 @@
+<?php
+include('Data_Access.php');
+?>
+<html>
+    <body>
+        <h1>Lab 7 Demo 2: PHP DES test</h1>
+        <?php
+            $db = openDB();
+            $ret = getAllCatalog($db);
+            while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+                echo "productID = ". $row['productID'] . "\n";
+                echo "productName = ". $row['productName'] ."\n";
+                echo "productQuantity = ". $row['productQuantity'] ."\n";
+                echo "productPrice = ".$row['productPrice'] ."\n\n";
+             }
+             
+             $ret = getAllCustomer($db);
+             while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+                echo "userID = ". $row['userID'] . "\n";
+                echo "userName = ". $row['userName'] ."\n";
+                echo "userEmail = ". $row['userEmail']."\n";
+                echo "userFirstName = ".$row['userFirstName']."\n";
+                echo "userLastName = ".$row['userLastName']."\n";
+                echo "userPassword = ".$row['userPassword']."\n";
+                echo "userPhoneNumber = ".$row['userPhoneNumber']."\n\n";
+             }
+             closeDB($db);
+            
+        ?>
+    </body>
+</html>
+
